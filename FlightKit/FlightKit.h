4530f9f3ba975547b50a921ec2e644a424cd4812
Daniel Vancura <daniel_vancura@me.com>
2017-12-27 12:02:54 -0800

    Initial Commit

---
+++ FlightKit/FlightKit.h
@@ -0,0 +1,19 @@
+//
+//  FlightKit.h
+//  FlightKit
+//
+//  Created by Daniel Vancura on 12/27/17.
+//  Copyright © 2017 Daniel Vancura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlightKit.
+FOUNDATION_EXPORT double FlightKitVersionNumber;
+
+//! Project version string for FlightKit.
+FOUNDATION_EXPORT const unsigned char FlightKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlightKit/PublicHeader.h>
+
+
